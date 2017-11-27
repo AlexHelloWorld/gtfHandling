df5018fc9e7bfea454555d6f004f758dbebf034a
Hongyu Shi <hongyushi@Hongyus-MacBook-Pro.local>
2017-11-27 23:39:42 +0800

    Add: make getInfoOfGenes more flexible with user defined attribute list

--- getInfoOfGenes.py
+++ getInfoOfGenes.py
@@ -2,53 +2,71 @@ import sys
 
 #get exon number (largest exon number among transcripts) from gtf files
 def processAttributeToDictionary(element8):
-    element8 = element8 = element8.replace('\"', '\t')
-    element8 = element8.replace(';', '\t')
-    elements = element8.strip().split()
+    elements = element8.strip().split(';')
     attrDict = dict()
-    for i in range(1, len(elements)/2):
-        attrDict[elements[2*i-2]] = elements[2*i-1]
+    for i in elements:
+        if(i != ''):
+            attr = i.split('"')
+            attr_name = attr[0].strip()
+            attr_value = attr[1].strip()
+            attrDict[attr_name] = attr_value
+
     return attrDict
 
 
-def getInfoOfGenes(inputFileName, outputFileName):
+#read a input GTF and output a tad-delimit file containing gene information
+def getInfoOfGenes(inputFileName, outputFileName, attributeList):
     inputFile = open(inputFileName, 'r')
     outputFile = open(outputFileName, 'a+')
-    geneIdHold = 'gene_id'
-    geneNameHold = 'gene_name'
-    geneTypeHold = 'gene_type'
-    chrHold = "chromosome"
-    startHold = "start"
-    endHold = "end"
-    exonNumberHold = 'exon_number'
+
+    #consctruct a dictionary containing information of a gene for output
+    infoDict = dict()
+    infoDict['chromosome'] = 'chromosome'
+    infoDict['start'] = 'start'
+    infoDict['end'] = 'end'
+    infoDict['exon_number'] = 'exon_number'
+    for attr in attributeList:
+        infoDict[attr] = attr
+
     for line in inputFile:
         if(line[0] != '#'):
             #get attribute dictionary
             elements = line.strip().split('\t')
             attrDict = processAttributeToDictionary(elements[8])
-            geneId = attrDict['gene_id']
+            geneId = attrDict[attributeList[0]]
             #if meet a new gene
-            if geneIdHold != geneId:
+            if infoDict[attributeList[0]] != geneId:
                 #output current hold values
-                outputFile.write(geneIdHold + '\t' + geneNameHold + '\t' + chrHold + '\t' + startHold + '\t' + endHold + '\t' + geneTypeHold + '\t' + str(exonNumberHold) + '\n')
-                #update gene id hold
-                geneIdHold = geneId
-                #updata other attributes
-                geneNameHold = attrDict['gene_name']
-                geneTypeHold = attrDict['gene_type']
-                chrHold = elements[0]
-                startHold = elements[3]
-                endHold = elements[4]
-                exonNumberHold = 0
+                #construct output string 
+                outputString = infoDict['chromosome'] + '\t' + infoDict['start'] + '\t' + infoDict['end'] + '\t' + str(infoDict['exon_number'])
+                for attr in attributeList:
+                    outputString = outputString + '\t' + infoDict[attr]
+                outputString = outputString + '\n'
+                outputFile.write(outputString)
+                #updata main attributes
+                infoDict['chromosome'] = elements[0]
+                infoDict['start'] = elements[3]
+                infoDict['end'] = elements[4]
+                infoDict['exon_number'] = 0
+                for attr in attributeList:
+                    try:
+                        infoDict[attr] = attrDict[attr]
+                    except Exception, e:
+                        infoDict[attr] = 'NA'    
             else:
                 #try to update exon number
-                if 'exon_number' in attrDict and exonNumberHold < int(attrDict['exon_number']):
-                    exonNumberHold = int(attrDict['exon_number'])
-    outputFile.write(geneIdHold + '\t' + geneNameHold + '\t' + chrHold + '\t' + startHold + '\t' + endHold + '\t' + geneTypeHold + '\t' + str(exonNumberHold) + '\n')
+                if 'exon_number' in attrDict and infoDict['exon_number'] < int(attrDict['exon_number']):
+                    infoDict['exon_number'] = int(attrDict['exon_number'])
+    outputString = infoDict['chromosome'] + '\t' + infoDict['start'] + '\t' + infoDict['end'] + '\t' + str(infoDict['exon_number'])
+    for attr in attributeList:
+        outputString = outputString + '\t' + infoDict[attr]
+    outputString = outputString
+    outputFile.write(outputString)
 
 def main(argv):
     output = argv[1] + ".geneInfo.txt"
-    getInfoOfGenes(argv[1], output)
+    inputList = argv[2:]
+    getInfoOfGenes(argv[1], output, inputList)
 
 
 if __name__ == "__main__":
